1133004ba40e368ea22f9881c50ec0d02f1ea4de
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-11-05 20:42:07 +0000

    Add files

---
+++ Include/CodeSmithy/BuildFiles.h
@@ -0,0 +1,12 @@
+/*
+    Copyright (c) 2020 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/CodeSmithyIDE/BuildFiles/blob/master/LICENSE.txt
+*/
+
+#ifndef _CODESMITHYIDE_BUILDFILES_H_
+#define _CODESMITHYIDE_BUILDFILES_H_
+
+#include "BuildFiles/linkoptions.h"
+
+#endif

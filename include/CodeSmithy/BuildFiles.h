593a9d1adce2a52bbe942429b4330950e9e5adf4
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2023-11-26 17:09:47 +0000

    Updates

--- include/CodeSmithy/BuildFiles.h
+++ include/CodeSmithy/BuildFiles.h
@@ -1,16 +1,15 @@
 /*
-    Copyright (c) 2020 Xavier Leclercq
+    Copyright (c) 2020-2023 Xavier Leclercq
     Released under the MIT License
-    See https://github.com/CodeSmithyIDE/BuildFiles/blob/master/LICENSE.txt
+    See https://github.com/codesmithyide/build-files/blob/main/LICENSE.txt
 */
 
-#ifndef _CODESMITHYIDE_BUILDFILES_H_
-#define _CODESMITHYIDE_BUILDFILES_H_
+#ifndef GUARD_CODESMITHYIDE_BUILDFILES_H
+#define GUARD_CODESMITHYIDE_BUILDFILES_H
 
+#include "BuildFiles/linkoptions.h"
 #include "BuildFiles/MSBuildFiltersFile.h"
 #include "BuildFiles/MSBuildProjectFile.h"
 #include "BuildFiles/VisualStudioSolutionFile.h"
-
-#include "BuildFiles/linkoptions.h"
 
 #endif
--- src/Bakefile/BakefileParser.cpp
+++ src/Bakefile/BakefileParser.cpp
@@ -1,40 +1,21 @@
 /*
     Copyright (c) 2017-2023 Xavier Leclercq
-
-    Permission is hereby granted, free of charge, to any person obtaining a
-    copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-    IN THE SOFTWARE.
+    Released under the MIT License
+    See https://github.com/codesmithyide/build-files/blob/main/LICENSE.txt
 */
 
 #include "Bakefile/BakefileParser.hpp"
 
-namespace CodeSmithy
-{
-
-BakefileParser::BakefileParser(std::istream& input)
+CodeSmithy::BakefileParser::BakefileParser(std::istream& input)
     : m_tokenizer(input)
 {
 }
 
-BakefileParser::~BakefileParser()
+CodeSmithy::BakefileParser::~BakefileParser()
 {
 }
 
-std::shared_ptr<Bakefile> BakefileParser::parse()
+std::shared_ptr<CodeSmithy::Bakefile> CodeSmithy::BakefileParser::parse()
 {
     std::shared_ptr<Bakefile> result = std::make_shared<Bakefile>();
 
@@ -44,6 +25,4 @@ std::shared_ptr<Bakefile> BakefileParser::parse()
     }
 
     return result;
-}
-
 }

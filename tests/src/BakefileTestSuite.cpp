550872f29fc7c2051fd54ded7df614965141d67e
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-03 00:50:51 +0000

    Updates

--- tests/src/BakefileTestSuite.cpp
+++ tests/src/BakefileTestSuite.cpp
@@ -1,8 +1,5 @@
-/*
-    Copyright (c) 2023 Xavier Leclercq
-    Released under the MIT License
-    See https://github.com/codesmithyide/build-files/blob/main/LICENSE.txt
-*/
+// SPDX-FileCopyrightText: 2017-2024 Xavier Leclercq
+// SPDX-License-Identifier: MIT
 
 #include "BakefileTestSuite.hpp"
 #include "BakefileTestSuite/BakefileTargetTests.h"

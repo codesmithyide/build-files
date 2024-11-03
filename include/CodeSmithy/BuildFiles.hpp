7d9417a06e3d7ed0163b3b1839952e9624e11c80
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-03 14:27:16 +0000

    Rename files

---
+++ include/CodeSmithy/BuildFiles.hpp
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2017-2024 Xavier Leclercq
+// SPDX-License-Identifier: MIT
+
+#ifndef GUARD_CODESMITHYIDE_BUILDFILES_HPP
+#define GUARD_CODESMITHYIDE_BUILDFILES_HPP
+
+#include "BuildFiles/linkoptions.hpp"
+#include "BuildFiles/MSBuildFiltersFile.hpp"
+#include "BuildFiles/MSBuildProjectFile.hpp"
+#include "BuildFiles/VisualStudioSolutionFile.hpp"
+
+#endif
---
+++ include/CodeSmithy/BuildFiles/linkoptions.hpp
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2017-2024 Xavier Leclercq
+// SPDX-License-Identifier: MIT
+
+#ifndef GUARD_CODESMITHYIDE_BUILDFILES_LINKOPTIONS_HPP
+#define GUARD_CODESMITHYIDE_BUILDFILES_LINKOPTIONS_HPP
+
+#ifdef _DEBUG
+#pragma comment(lib, "CodeSmithyBuildFiles-d.lib")
+#else
+#pragma comment(lib, "CodeSmithyBuildFiles.lib")
+#endif
+
+#endif

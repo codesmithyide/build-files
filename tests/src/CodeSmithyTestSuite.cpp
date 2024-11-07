b05192e32a0e47563e48d83b5c49a91651e997fb
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-07 12:36:35 +0000

    Updates

--- tests/src/CodeSmithyTestSuite.cpp
+++ tests/src/CodeSmithyTestSuite.cpp
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: MIT
 
 #include "CodeSmithyTestSuite.hpp"
-#include "CodeSmithyTestSuite/CodeSmithyProjectFileTests.hpp"
-#include "CodeSmithyTestSuite/CodeSmithyProjectFileXMLRepositoryTests.hpp"
+#include "CodeSmithyTestSuite/CodeSmithyBuildFileTests.hpp"
+#include "CodeSmithyTestSuite/CodeSmithyBuildFileXMLRepositoryTests.hpp"
 
 CodeSmithyTestSuite::CodeSmithyTestSuite(const Ishiko::TestNumber& number, const Ishiko::TestContext& context)
     : Ishiko::TestSequence(number, "CodeSmithy test suite", context)

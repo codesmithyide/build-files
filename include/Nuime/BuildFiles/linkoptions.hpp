ede18087885867be6ecfd6602f0a9039615eb4ef
Xavier Leclercq <xavier@leclercq.dev>
2026-07-27 15:50:55 +0100

    Updates

--- include/Nuime/BuildFiles/linkoptions.hpp
+++ include/Nuime/BuildFiles/linkoptions.hpp
@@ -1,8 +1,8 @@
 // SPDX-FileCopyrightText: 2017-2024 Xavier Leclercq
 // SPDX-License-Identifier: MIT
 
-#ifndef GUARD_CODESMITHYIDE_BUILDFILES_LINKOPTIONS_HPP
-#define GUARD_CODESMITHYIDE_BUILDFILES_LINKOPTIONS_HPP
+#ifndef GUARD_NUIME_BUILDFILES_LINKOPTIONS_HPP
+#define GUARD_NUIME_BUILDFILES_LINKOPTIONS_HPP
 
 #ifdef _DEBUG
 #pragma comment(lib, "CodeSmithyBuildFiles-d.lib")

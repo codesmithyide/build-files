5e2a92785e0e2fd12a2fdf083d40fc92a4d6aded
Xavier Leclercq <xavier@leclercq.dev>
2026-07-27 13:37:45 +0100

    Update Bakefile.hpp

--- include/Nuime/BuildFiles/Bakefile/Bakefile.hpp
+++ include/Nuime/BuildFiles/Bakefile/Bakefile.hpp
@@ -1,8 +1,8 @@
-// SPDX-FileCopyrightText: 2017-2024 Xavier Leclercq
+// SPDX-FileCopyrightText: 2017-2026 Xavier Leclercq
 // SPDX-License-Identifier: MIT
 
-#ifndef GUARD_CODESMITHYIDE_BUILDFILES_BAKEFILE_BAKEFILE_HPP
-#define GUARD_CODESMITHYIDE_BUILDFILES_BAKEFILE_BAKEFILE_HPP
+#ifndef GUARD_NUIME_BUILDFILES_BAKEFILE_BAKEFILE_HPP
+#define GUARD_NUIME_BUILDFILES_BAKEFILE_BAKEFILE_HPP
 
 #include "BakefileTarget.hpp"
 #include <vector>

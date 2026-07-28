81147da7846fa6e36e441aa254576476cb255f34
Xavier Leclercq <xavier@leclercq.dev>
2026-07-28 14:05:30 +0100

    Updates

--- tests/src/NuimeTestSuite/NuimeBuildFileTests.cpp
+++ tests/src/NuimeTestSuite/NuimeBuildFileTests.cpp
@@ -13,6 +13,7 @@ NuimeBuildFileTests::NuimeBuildFileTests(const Ishiko::TestNumber& number, const
     append<Ishiko::HeapAllocationErrorsTest>("Constructor test 1", ConstructorTest1);
     append<Ishiko::HeapAllocationErrorsTest>("Constructor test 2", ConstructorTest2);
     append<Ishiko::HeapAllocationErrorsTest>("load test 1", LoadTest1);
+    append<Ishiko::HeapAllocationErrorsTest>("load test 2", LoadTest2);
 }
 
 void NuimeBuildFileTests::ConstructorTest1(Ishiko::Test& test)
@@ -57,5 +58,24 @@ void NuimeBuildFileTests::LoadTest1(Ishiko::Test& test)
     ISHIKO_TEST_FAIL_IF_NEQ(build_file.recipes()[0].inputs()[0].asString(), "src/main.cpp");
     ISHIKO_TEST_FAIL_IF_NEQ(build_file.recipes()[0].outputs().size(), 1);
     ISHIKO_TEST_FAIL_IF_NEQ(build_file.recipes()[0].outputs()[0].asString(), "example");
+    ISHIKO_TEST_PASS();
+}
+
+void NuimeBuildFileTests::LoadTest2(Ishiko::Test& test)
+{
+    boost::filesystem::path input_path = test.context().getDataPath("minimal_with_label.nuime");
+
+    NuimeBuildFile build_file;
+
+    Ishiko::Error error;
+    build_file.load(input_path, error);
+
+    ISHIKO_TEST_FAIL_IF(error);
+    ISHIKO_TEST_FAIL_IF_NEQ(build_file.recipes().size(), 1);
+    ISHIKO_TEST_FAIL_IF_NEQ(build_file.recipes()[0].target().name(), "lib");
+    ISHIKO_TEST_FAIL_IF_NEQ(build_file.recipes()[0].target().labels().size(), 1);
+    ISHIKO_TEST_FAIL_IF_NEQ(build_file.recipes()[0].target().labels()[0].asString(), "nuime:product");
+    ISHIKO_TEST_FAIL_IF_NEQ(build_file.recipes()[0].inputs().size(), 1);
+    ISHIKO_TEST_FAIL_IF_NEQ(build_file.recipes()[0].outputs().size(), 1);
     ISHIKO_TEST_PASS();
 }
